76009d7030f202c65525a491e4ccb6dc1ddc59c8
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2019-12-09 09:35:29 +0100

    App.py integrated in pi.py
    Ayy lmao

--- pi.py
+++ pi.py
@@ -10,10 +10,50 @@ from PIL import Image, ImageChops
 import socket
 import requests
 import boto3
+from random import randint
+from time import strftime
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 globals()['upcode'] = 0
 globals()['boxlist'] = () #Da tuple lmao
 globals()['colNocr'] = 0
+
+#Setting up the interface for selecting box order
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+    surname = TextField('Surname:', validators=[validators.required()])
+    
+def get_time():
+    time = strftime("%Y-%m-%dT%H:%M")
+    return time
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+
+    if request.method == 'POST':
+        sort_type=request.form['sorttype']
+        box1=request.form['box1']
+        box2=request.form['box2']
+        box3=request.form['box3']
+        box4=request.form['box4']
+        box5=request.form['box5']
+        box6=request.form['box6']
+        box7=request.form['box7']
+
+        flash('Sorting for: [{}] Box 1 : ({}),Box 2 : ({}), Box 3 : ({}), Box 4 : ({}), Box 5 : ({}), Box 6 : ({}), Box 7 : ({}),'.format(sort_type, box1, box2, box3, box4, box5, box6, box7))
+
+    return render_template('index.html', form=form)
+
+if __name__ == "__main__":
+    app.run()
+
 
 def resizer(src, dest):
     im = Image.open(src)
